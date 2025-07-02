c683e5b7c4d9ea53e3617bdeba50d3eb67bf2bfa
Lucas Martins <lucasmartinscaet@hotmail.com>
2025-07-01 22:52:26 -0300

    ex01: creating class Book with random isbn

--- fundamentals/exercises/ex01-bookstore/src/Book.java
+++ fundamentals/exercises/ex01-bookstore/src/Book.java
@@ -1,2 +1,43 @@
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
+
 public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private LocalDate datePublished;
+    private int quantityAvailable;
+
+    public Book(String title, String author, LocalDate datePublished, int quantityAvailable){
+        this.isbn = UUID.randomUUID().toString();
+        this.author = author;
+        this.datePublished = datePublished;
+        this.quantityAvailable = quantityAvailable;
+    }
+
+    public String getIsbn(){
+        return this.isbn;
+    }
+
+    public String getAuthor(){
+        return this.author;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public LocalDate getDatePublished(){
+        return this.datePublished;
+    }
+
+    public void setDatePublished(LocalDate datePublished){
+        this.datePublished = datePublished;
+    }
+
+    public boolean isAvailable(){
+        return this.quantityAvailable > 0;
+    }
 }
--- fundamentals/src/Main.java
+++ fundamentals/src/Main.java
@@ -1,15 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Car bmw = new Car("BMW");
+        Car gol = new Car("GOL");
+        gol.gas();
+    }
+}
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+class Car {
+    private String model;
+
+    Car(String model){
+        this.model = model;
+    }
+
+    public void gas(){
+        System.out.println(model);
     }
 }
